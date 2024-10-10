62af7de4238c3a751e15e24e3a89939ece762334
MonashAmy <116072874+MonashAmy@users.noreply.github.com>
2024-10-10 12:05:36 +1100

    Commit - .js and .html

--- charts.js
+++ charts.js
@@ -1,2 +1,20 @@
 var vg_2 = "graph2.vg.json";
 vegaEmbed("#graph2", vg_2).then(function(result) {}).catch(console.error);
+
+var vg_3 = "graph3.vg.json";
+vegaEmbed("#graph3", vg_3).then(function(result) {}).catch(console.error);
+
+var vg_4 = "graph4.vg.json";
+vegaEmbed("#graph4", vg_4).then(function(result) {}).catch(console.error);
+
+var vg_5 = "graph5.vg.json";
+vegaEmbed("#graph5", vg_5).then(function(result) {}).catch(console.error);
+
+var vg_6 = "graph6.vg.json";
+vegaEmbed("#graph6", vg_6).then(function(result) {}).catch(console.error);
+
+var vg_7 = "graph7.vg.json";
+vegaEmbed("#graph7", vg_7).then(function(result) {}).catch(console.error);
+
+var vg_8 = "graph8.vg.json";
+vegaEmbed("#graph8", vg_8).then(function(result) {}).catch(console.error);
